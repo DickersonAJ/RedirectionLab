c4fad9888c970cbe4fd15efa11fe7ced23b179cb
DickersonAJ <>
2021-01-06 00:51:27 -0500

    added and tested MakeStars with normal and strange indentation and spaces

---
+++ MakeStars.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class MakeStars {
+  public static void main(String[]args){
+    Scanner n = new Scanner(System.in);
+    String l = "";
+    Scanner linear = n;
+    String s = "";
+    while (n.hasNextLine()) {
+      linear = new Scanner(n.nextLine());
+      while (linear.hasNext()) {
+        s = linear.next();
+        for (int i = 0; i < s.length(); i++) {
+          System.out.print("*");
+        }
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
+  }
+}
